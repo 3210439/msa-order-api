b45e7b50e8bb745c05ae23a339b795926bcd299d
Bell_Bell <47379176+3210439@users.noreply.github.com>
2025-03-31 22:11:47 +0900

    프로메테우스 데이터 수집을 위한 초기 설정 추가 및 스웨거 의존성 수정

--- src/main/java/com/ecommerce/order/controller/OrderController.java
+++ src/main/java/com/ecommerce/order/controller/OrderController.java
@@ -3,15 +3,12 @@ package com.ecommerce.order.controller;
 import com.ecommerce.order.event.OrderEventPublisher;
 import com.ecommerce.order.model.Order;
 import com.ecommerce.order.service.OrderService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
-@Api(value = "Order Service API")
 public class OrderController {
     @Autowired
     private OrderEventPublisher orderEventPublisher;
@@ -20,9 +17,7 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping
-    @ApiOperation(value = "Create a new order")
     public ResponseEntity<String> createOrder(@RequestBody Order order) {
-
         order.setStatus("PENDING");
         orderService.createOrder(order);
         orderEventPublisher.publishOrderCreated(order);
@@ -31,7 +26,6 @@ public class OrderController {
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "Get order by ID")
     public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
         return ResponseEntity.ok(orderService.getOrderById(id));
     }
